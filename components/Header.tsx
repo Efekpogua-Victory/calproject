f06c094642cafcebbf4f2b149c417c59180ffed7
Victory33404 <efekpogua_victory@yahoo.com>
2022-04-10 22:08:29 +0100

    created required database models

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,11 +7,14 @@ const Header = () => {
     return (
         <nav className="bg-black px-2 sm:px-4 py-2.5">
             <div className="container flex flex-wrap justify-between items-center mx-auto">
-                <a href="" className="flex items-center">
-                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                        Calc.com
-                    </span>
-                </a>
+                <Link href="/" >
+                    <a className="flex items-center">
+                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                            Calc.com
+                        </span>
+                    </a>
+                </Link>
+
 
                 <div className="w-full md:block md:w-auto">
                     <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
