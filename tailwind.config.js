4e9fb3f7ce9dd5c61f6c5d21351ad3677db95606
Victory33404 <efekpogua_victory@yahoo.com>
2022-04-13 20:27:09 +0100

    configuring tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /* eslint-disable prettier/prettier */
 module.exports = {
-    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [
-
-    ],
-}
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: false,
+  },
+  plugins: [require("daisyui")],
+};
