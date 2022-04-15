2fb5a13c9e5f27deee6b587129f6fb1fc88c59ee
Victory33404 <efekpogua_victory@yahoo.com>
2022-04-15 11:55:52 +0100

    .

--- pages/api/event_types/addevent.tsx
+++ pages/api/event_types/addevent.tsx
@@ -1,28 +1,28 @@
-/* eslint-disable prettier/prettier */
-import { NextApiRequest, NextApiResponse } from "next";
-import { getSession } from "next-auth/react";
+// /* eslint-disable prettier/prettier */
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { getSession } from "next-auth/react";
 
-import prisma from "@helpers/prisma";
+// import prisma from "@helpers/prisma";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const session = await getSession({ req });
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   const session = await getSession({ req });
 
-  if (session) {
-    if (req.method !== "POST") {
-      return;
-    }
+//   if (session) {
+//     if (req.method !== "POST") {
+//       return;
+//     }
 
-    const data = req.body;
+//     const data = req.body;
 
-    try {
-      await prisma.event.create({
-        data: {},
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(403).json({ err: "Error occured while adding a new food." });
-    }
+//     try {
+//       await prisma.event.create({
+//         data: {},
+//       });
+//     } catch (err) {
+//       console.log(err);
+//       res.status(403).json({ err: "Error occured while adding a new food." });
+//     }
 
-    res.status(201).json({ message: "Event Added" });
-  }
-}
+//     res.status(201).json({ message: "Event Added" });
+//   }
+// }
